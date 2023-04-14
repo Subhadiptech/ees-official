e3235a311b9b49fffbe4de0a4da104c10687bebc
Subhadiptech <subhadipdhn@gmail.com>
2023-04-15 01:36:55 +0530

    init project

---
+++ ees/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <div className="">
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ees/src/components/Prof.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Prof() {
+  return (
+    <div>Prof</div>
+  )
+}
